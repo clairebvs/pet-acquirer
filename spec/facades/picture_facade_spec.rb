afdbdd4e1d52d61f1c6a9c252ab907017a0a8fad
ClaireBvs <clairembeauvais@gmail.com>
2019-01-05 21:49:45 -0700

    Add facades test for PictureFacade [#1]

---
+++ spec/facades/picture_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe PictureFacade do
+  subject { PictureFacade.new }
+
+  it 'exists' do
+    expect(subject).to be_a(PictureFacade)
+  end
+
+  context 'instance methods' do
+    context '#random_picture' do
+      it 'returns a random picture' do
+        expect(subject.random_picture.first).to be_a(Picture)
+      end
+    end
+  end
+end
