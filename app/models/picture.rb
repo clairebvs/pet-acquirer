7b9f4c87c1f176133263a1e45dce66baf49433c2
ClaireBvs <clairembeauvais@gmail.com>
2019-01-05 21:34:17 -0700

    Add method to make api call petfinder [#1]

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,9 @@ class Picture
   attr_reader :name, :photo, :breed
 
   def initialize(picture_data)
-    @name = picture_data["name"]
-    @photo = picture_data["photos"]
-    @breed = picture_data["breeds"]
+    @name = picture_data[:name][:$t]
+    @photo = picture_data[:media][:photos][:photo][0][:$t]
+    @breed = picture_data[:breeds][:breed][:$t]
   end
 
   def self.find_random_picture
@@ -16,9 +16,9 @@ class Picture
 
     response = @conn.get("pet.getRandom?key=#{api_key}&format=json&output=basic")
 
-    results = JSON.parse(response.body, symbolize_names: true)[:petfinder][:pet]
+    results = JSON.parse(response.body, symbolize_names: true)[:petfinder]
 
-    results.map do |result|
+    results.map do |pet, result|
       Picture.new(result)
     end
   end
