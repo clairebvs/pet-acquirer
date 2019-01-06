e6cf6136f97f9a596a979d5c9c14b3f381c58608
ClaireBvs <clairembeauvais@gmail.com>
2019-01-05 20:12:50 -0700

    Add model test #photo and #breed for Picture [#1]

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -18,7 +18,19 @@ describe Picture do
   context "instance methods" do
     context "#name" do
       it "returns a name" do
-        expect(subject.name).to eq("UDR")
+        expect(subject.name).to eq("Duke")
+      end
+    end
+
+    context "#photo" do
+      it "returns a photo url" do
+        expect(subject.photo).to eq("http://photos.petfinder.com/photos/pets/42878117/1/?bust=1538149801&width=500&-x.jpg")
+      end
+    end
+
+    context "#breed" do
+      it "returns a breed name" do
+        expect(subject.breed).to eq("German Shepherd Dog")
       end
     end
   end
