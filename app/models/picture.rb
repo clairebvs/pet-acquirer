18d8b00072e3c779bde1197b6bede6d6b7691d88
ClaireBvs <clairembeauvais@gmail.com>
2019-01-05 20:24:55 -0700

    Add #find_random_picture to Picture [#1]

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -8,6 +8,18 @@ class Picture
   end
 
   def self.find_random_picture
+    @conn = Faraday.new(url: "http://api.petfinder.com") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
 
+    api_key = ENV["pet_api_key"]
+
+    response = @conn.get("pet.getRandom?key=#{api_key}&format=json&output=basic")
+
+    results = JSON.parse(response.body, symbolize_names: true)[:petfinder][:pet]
+
+    results.map do |result|
+      Picture.new(result)
+    end
   end
 end
