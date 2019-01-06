2be45d9c61cd6280a3fd184611467417899a123c
ClaireBvs <clairembeauvais@gmail.com>
2019-01-05 22:19:05 -0700

    Remove #find_random_picture from Picture [#1]

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,24 +2,9 @@ class Picture
   attr_reader :name, :photo, :breed
 
   def initialize(picture_data)
+    require "pry"; binding.pry
     @name = picture_data[:name][:$t]
     @photo = picture_data[:media][:photos][:photo][0][:$t]
     @breed = picture_data[:breeds][:breed][:$t]
-  end
-
-  def self.find_random_picture
-    @conn = Faraday.new(url: "http://api.petfinder.com") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-
-    api_key = ENV["pet_api_key"]
-
-    response = @conn.get("pet.getRandom?key=#{api_key}&format=json&output=basic")
-
-    results = JSON.parse(response.body, symbolize_names: true)[:petfinder]
-
-    results.map do |pet, result|
-      Picture.new(result)
-    end
   end
 end
