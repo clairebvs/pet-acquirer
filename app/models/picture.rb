ffbcf569236fee85d448301dcb0d8d9d3bbce4c7
ClaireBvs <clairembeauvais@gmail.com>
2019-01-06 19:15:37 -0700

    Add #breed_check and #name_check to Picture [#1]

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,17 +1,33 @@
 class Picture
-  attr_reader :name, :breed
+  attr_reader :name, :breed, :photo
 
   def initialize(picture_data)
-    @name = picture_data["name"]["$t"]
-    @photo = photo(picture_data)
-    @breed = picture_data["breeds"]["breed"]["$t"]
+    @name = name_check(picture_data)
+    @photo = photo_check(picture_data)
+    @breed = breed_check(picture_data)
   end
 
-  def photo(picture_data)
+  def photo_check(picture_data)
     if picture_data["media"]["photos"]["photo"].class == Array
       picture_data["media"]["photos"]["photo"][0]["$t"]
     else
       picture_data["media"]["photos"]["photo"]["$t"]
+    end
+  end
+
+  def breed_check(picture_data)
+    if picture_data["breeds"]["breed"].class == Array
+      picture_data["breeds"]["breed"][0]["$t"]
+    else
+      picture_data["breeds"]["breed"]["$t"]
+    end
+  end
+
+  def name_check(picture_data)
+    if picture_data["name"].present?
+      picture_data["name"]["$t"]
+    else
+      "None yet"
     end
   end
 end
