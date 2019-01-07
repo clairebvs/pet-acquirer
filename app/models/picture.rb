6aff4aa987bb65a3861938dd209d464c1e4f5f8b
ClaireBvs <clairembeauvais@gmail.com>
2019-01-06 18:49:24 -0700

    Add #photo to Picture to check for array [#1]

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,17 @@
 class Picture
-  attr_reader :name, :photo, :breed
+  attr_reader :name, :breed
 
   def initialize(picture_data)
     @name = picture_data["name"]["$t"]
-    @photo = picture_data["media"]["photos"]["photo"][0]["$t"]
+    @photo = photo(picture_data)
     @breed = picture_data["breeds"]["breed"]["$t"]
+  end
+
+  def photo(picture_data)
+    if picture_data["media"]["photos"]["photo"].class == Array
+      picture_data["media"]["photos"]["photo"][0]["$t"]
+    else
+      picture_data["media"]["photos"]["photo"]["$t"]
+    end
   end
 end
