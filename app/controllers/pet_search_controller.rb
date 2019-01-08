27e6439cac42eb9776d34402ae736ae4e4c3309d
ClaireBvs <clairembeauvais@gmail.com>
2019-01-07 21:30:49 -0700

    Add #pet_search_params in PetSearchController [#2]

--- app/controllers/pet_search_controller.rb
+++ app/controllers/pet_search_controller.rb
@@ -1,4 +1,10 @@
 class PetSearchController < ApplicationController
   def index
+    @pet_find = PetFacade.new(pet_search_params)
+  end
+
+  private
+  def pet_search_params
+    params.permit(:location, :animal, :age, :size)
   end
 end
