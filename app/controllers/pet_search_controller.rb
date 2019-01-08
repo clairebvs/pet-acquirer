9321b309d667dccefddd9f10a52e51f30ec39c15
ClaireBvs <clairembeauvais@gmail.com>
2019-01-08 16:19:38 -0700

    Add logic to see if params[location] is valid PetSearchCOntroller [#13]

--- app/controllers/pet_search_controller.rb
+++ app/controllers/pet_search_controller.rb
@@ -1,7 +1,12 @@
 class PetSearchController < ApplicationController
   def index
     if params["location"]
-      @pet_find = PetFacade.new(pet_search_params).pet_found
+      if params["location"].present?
+        @pet_find = PetFacade.new(pet_search_params).pet_found
+      else
+        flash[:error] = 'Location and Animal fields are required'
+        redirect_to '/pet_search'
+      end
     end
   end
 
