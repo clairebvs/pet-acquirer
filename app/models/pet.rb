306d83368f9584b4fb1c959688e4fff8c339ea6b
ClaireBvs <clairembeauvais@gmail.com>
2019-01-08 13:53:29 -0700

    Add #photo, #size, #age, #location in Pet [#11]

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,9 +2,22 @@ class Pet
   attr_reader :name, :photo, :size, :age, :location
 
   def initialize(pet_data)
-
     @name = pet_data["name"]["$t"]
-    # @photo = photo_check(pet_data)
-    # @breed = breed_check(pet_data)
+    @photo = photo_check(pet_data)
+    @size = pet_data["size"]["$t"]
+    @age = pet_data["age"]["$t"]
+    @location = city_state(pet_data)
+  end
+
+  def photo_check(pet_data)
+    if pet_data["media"]["photos"]["photo"].class == Array
+      pet_data["media"]["photos"]["photo"][0]["$t"]
+    else
+      pet_data["media"]["photos"]["photo"]["$t"]
+    end
+  end
+
+  def city_state(pet_data)
+    pet_data["contact"]["city"]["$t"] + pet_data["contact"]["state"]["$t"]
   end
 end
