a0940550c47425314af2c0a485fe7ddd83487fd5
ClaireBvs <clairembeauvais@gmail.com>
2019-01-16 17:08:22 -0700

    Add #initilize and #all_pets for PetFinderService [#31]

--- app/services/pet_finder_service.rb
+++ app/services/pet_finder_service.rb
@@ -1,6 +1,14 @@
 class PetFinderService
+  def initialize(pet_params = {})
+    @pet_params = pet_params
+  end
+
   def pictures
    get_json("pet.getRandom?key=#{api_key}&format=json&output=basic")["petfinder"]
+  end
+
+  def all_pets
+    get_json("/pet.find?key=#{api_key}&format=json&location=#{@pet_params["location"]}&animal=#{@pet_params["animal"]}&size=#{@pet_params["size"]}&age=#{@pet_params["age"]}")
   end
 
   private
